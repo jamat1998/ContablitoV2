3fe64149b0b2df7a0ff30ebb29be790c54c9fe05
Gustavo <75509324+guskpo20@users.noreply.github.com>
2022-09-25 13:23:52 -0300

    localStorage con buggs
    El stock lo guarda y toma bien, pero como que no lo tiene en cuenta a la hora de los movimientos y crea algo con la misma id (tengo que leer bien como lo hizo mathias).
    Los movimientos entendí menos, guarda algunos datos y otros terminan undefined

--- dynamic.js
+++ dynamic.js
@@ -1,28 +1,37 @@
-const RegisterProductName = document.getElementById("RegisterProductName");
-const registerCodeProduct = document.getElementById("registerCodeProduct");
-const description = document.getElementById("description");
-const stock = document.getElementById("stock");
-const inventoryCostUnit = document.getElementById("inventoryCostUnit");
-const formInventory = document.getElementById("formInventory");
-const bodyTableInventory = document.getElementById("bodyTableInventory");
+const RegisterProductName = document.getElementById('RegisterProductName');
+const registerCodeProduct = document.getElementById('registerCodeProduct');
+const description = document.getElementById('description');
+const stock = document.getElementById('stock');
+const inventoryCostUnit = document.getElementById('inventoryCostUnit');
+const formInventory = document.getElementById('formInventory');
+const bodyTableInventory = document.getElementById('bodyTableInventory');
 
-const sell = document.getElementById("sell");
-const buy = document.getElementById("buy");
-const movementProductName = document.getElementById("movementProductName");
-const movementCodeProduct = document.getElementById("movementCodeProduct");
-const date = document.getElementById("date");
-const count = document.getElementById("count");
-const basico = document.getElementById("basico");
-const minimo = document.getElementById("minimo");
-const exento = document.getElementById("exento");
-const movementCostUnit = document.getElementById("movementCostUnit");
-const totalCost = document.getElementById("movementCostUnit");
-const formMovement = document.getElementById("formMovement");
-const bodyTableMovement = document.getElementById("bodyTableMovement");
+const sell = document.getElementById('sell');
+const buy = document.getElementById('buy');
+const movementProductName = document.getElementById('movementProductName');
+const movementCodeProduct = document.getElementById('movementCodeProduct');
+const date = document.getElementById('date');
+const count = document.getElementById('count');
+const basico = document.getElementById('basico');
+const minimo = document.getElementById('minimo');
+const exento = document.getElementById('exento');
+const movementCostUnit = document.getElementById('movementCostUnit');
+const totalCost = document.getElementById('movementCostUnit');
+const formMovement = document.getElementById('formMovement');
+const bodyTableMovement = document.getElementById('bodyTableMovement');
 const arrayElements = [];
 const positionElement = arrayElements.indexOf(movementCodeProduct.value);
 
 function innerInventory(containeer, code, name, stock, costUnity) {
+  let product = {
+    id: code,
+    name: name,
+    description: description.value,
+    stock: stock,
+    costUnit: costUnity,
+    inventory: true,
+  };
+  localStorage.setItem(code, JSON.stringify(product));
   containeer.innerHTML += `
     <tr class='el ${alertsStockInitial(stock)}'>
     <td>${code}</td>
@@ -34,6 +43,19 @@ function innerInventory(containeer, code, name, stock, costUnity) {
     `;
 }
 function innerMovements(transaccion, Iva, TotalWithIva, totalCost) {
+  let product = {
+    id: movementCodeProduct.value,
+    transaccion: transaccion,
+    date: date.value,
+    name: movementProductName.value,
+    count: count.value,
+    costUnit: movementCostUnit.value,
+    totalCost: totalCost,
+    iva: Iva,
+    TotalWithIva: TotalWithIva,
+    inventory: false,
+  };
+  localStorage.setItem(movementCodeProduct.value, JSON.stringify(product));
   bodyTableMovement.innerHTML += `
     <tr>
     <td>${movementCodeProduct.value}</td>
@@ -50,24 +72,24 @@ function innerMovements(transaccion, Iva, TotalWithIva, totalCost) {
 }
 function alertsStock(stock, positionElement) {
   if (stock > 500) {
-    bodyTableInventory.children[positionElement].classList.add("table-success");
+    bodyTableInventory.children[positionElement].classList.add('table-success');
   }
   if (stock <= 500 && stock >= 150) {
-    bodyTableInventory.children[positionElement].classList.add("table-warning");
+    bodyTableInventory.children[positionElement].classList.add('table-warning');
   }
   if (stock < 150) {
-    bodyTableInventory.children[positionElement].classList.add("table-danger");
+    bodyTableInventory.children[positionElement].classList.add('table-danger');
   }
 }
 function alertsStockInitial(stock) {
   if (stock > 500) {
-    return "table-success";
+    return 'table-success';
   }
   if (stock <= 500 && stock >= 150) {
-    return "table-warning";
+    return 'table-warning';
   }
   if (stock < 150) {
-    return "table-danger";
+    return 'table-danger';
   }
 }
 function newStockInner(newStock, position) {
@@ -94,7 +116,7 @@ function sells() {
   );
   let TotalWithivaExento = totalCost;
   if (bodyTableInventory.firstChild) {
-    let totalSells = document.getElementById("totalSells");
+    let totalSells = document.getElementById('totalSells');
     if (sell.checked && basico.checked) {
       innerMovements(sell.value, basico.value, TotalWithivaBasico, totalCost);
     }
@@ -119,18 +141,18 @@ function sells() {
     }
     if (sell.checked && arrayElements.includes(movementCodeProduct.value)) {
       const positionElement = arrayElements.indexOf(movementCodeProduct.value);
-      const element = document.querySelectorAll(".el");
+      const element = document.querySelectorAll('.el');
       const newStock =
         parseInt(element[positionElement].children[3].textContent) -
         parseInt(count.value);
       bodyTableInventory.children[positionElement].classList.remove(
-        "table-danger"
+        'table-danger'
       );
       bodyTableInventory.children[positionElement].classList.remove(
-        "table-warning"
+        'table-warning'
       );
       bodyTableInventory.children[positionElement].classList.remove(
-        "table-success"
+        'table-success'
       );
       if (newStock >= 0 && sell.checked) {
         newStockInner(newStock, positionElement);
@@ -168,7 +190,7 @@ function buys() {
     totalCost + (minimo.value * totalCost) / 100
   );
   let TotalWithivaExento = totalCost;
-  let totalBuys = document.getElementById("totalBuys");
+  let totalBuys = document.getElementById('totalBuys');
   if (buy.checked) {
     let cuantity = parseInt(count.value);
     let unityValue = parseInt(movementCostUnit.value);
@@ -206,25 +228,25 @@ function buys() {
   }
   if (buy.checked && arrayElements.includes(movementCodeProduct.value)) {
     const positionElement = arrayElements.indexOf(movementCodeProduct.value);
-    const element = document.querySelectorAll(".el");
+    const element = document.querySelectorAll('.el');
     const newStock =
       parseInt(element[positionElement].children[3].textContent) +
       parseInt(count.value);
     bodyTableInventory.children[positionElement].classList.remove(
-      "table-success"
+      'table-success'
     );
     bodyTableInventory.children[positionElement].classList.remove(
-      "table-warning"
+      'table-warning'
     );
     bodyTableInventory.children[positionElement].classList.remove(
-      "table-danger"
+      'table-danger'
     );
     newStockInner(newStock, positionElement);
     alertsStock(newStock, positionElement);
   }
 }
 
-formInventory.addEventListener("submit", (e) => {
+formInventory.addEventListener('submit', (e) => {
   e.preventDefault();
   if (arrayElements.includes(registerCodeProduct.value) === false) {
     arrayElements.push(registerCodeProduct.value);
@@ -236,11 +258,62 @@ formInventory.addEventListener("submit", (e) => {
       inventoryCostUnit.value
     );
   } else if (arrayElements.includes(registerCodeProduct.value)) {
-    alert("Ya tienes este articulo en el inventario");
+    alert('Ya tienes este articulo en el inventario');
   }
 });
-formMovement.addEventListener("submit", (e) => {
+formMovement.addEventListener('submit', (e) => {
   e.preventDefault();
   buys();
   sells();
 });
+
+// let perfilUsuario = { id:1, nombre:"juan", apellidos:"garcias garcias"}
+// localStorage.setItem('perfil', JSON.stringify(perfilUsurio))
+//let perfil = JSON.parse(localStorage.getItem('perfil'))
+
+function firstLoad() {
+  let products = allStorage();
+  for (let product of products) {
+    product = JSON.parse(product);
+    if (product.inventory) {
+      bodyTableInventory.innerHTML += `
+    <tr class='el ${alertsStockInitial(stock)}'>
+    <td>${product.id}</td>
+    <td>${product.name}</td>
+    <td>${product.description}</td>
+    <td>${product.stock}</td>
+    <td>$${product.costUnit}</td>
+    </tr>
+    `;
+    } else {
+      console.log(product);
+      bodyTableMovement.innerHTML += `
+    <tr>
+    <td>${product.id}</td>
+    <td>${product.transaccion}</td>
+    <td>${product.date}</td>
+    <td>${product.name}</td>
+    <td>${product.count}</td>
+    <td>$${product.costUnit}</td>
+    <td>$${product.totalCost}</td>
+    <td>${product.iva}%</td>
+    <td>$${product.TotalWithIva}</td>
+    </tr>
+    `;
+    }
+  }
+}
+
+function allStorage() {
+  var values = [],
+    keys = Object.keys(localStorage),
+    i = keys.length;
+
+  while (i--) {
+    values.push(localStorage.getItem(keys[i]));
+  }
+
+  return values;
+}
+
+firstLoad();
